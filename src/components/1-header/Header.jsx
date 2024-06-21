1064f716f70d086d87e5d73bfadce10dd43bafe4
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2024-06-21 04:14:44 +0300

    Header

---
+++ src/components/1-header/Header.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useState } from 'react';
+import  './Header.css';
+
+export const Header = () => {
+  const [showModal, setshowModal] = useState(false);
+  return (
+    <header className='flex' >
+      <button  onClick={() => {setshowModal(true)}} className='icon-menu menu' />
+
+      <div/>
+      <nav>
+        <ul className='flex' >
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      
+      <button className='mode flex'>
+        <span className='icon-moon-o' ></span>
+      </button>
+
+      {showModal && (
+        <div className=" fixed">   
+        <ul className="modal ">
+          <li>
+            <button className='icon-clear' onClick={() => {setshowModal(false)}} />
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Uses</a>
+          </li>
+        </ul>
+    </div>
+      )}
+    </header>
+  )
+}
+
+export default Header;
