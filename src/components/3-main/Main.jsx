ec0e3321625156ac3226e3871a4d2ab101b107ee
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2024-06-24 06:47:03 +0300

    Main

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-import './Main.css';
+import React from "react";
+import "./Main.css";
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <main className="flex">
+      <section className="flex left-section  ">
+        <button className="active">All Projects</button>
+        <button>HTML & CSS</button>
+        <button>JavaScript</button>
+        <button>React & MUI</button>
+        <button>Ruby On Rails</button>
+        <button>Node & Express</button>
+      </section>
+
+      <section className="right-section flex  ">
+        {["aa", "bb", "cc" ,"b", "c"].map((item) => {
+          return (
+            <article key={item} className="card">
+              <img
+                className=""
+                width={266}
+                src="public/resturant.jpeg"
+                alt="resturant"
+              />
+              <div className="box  " style={{ width: "266px" }}>
+                <h1 className="title">Landing Page2</h1>
+                <p className="sub-title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore inventore doloribus in quasi alias animi?
+                </p>
+
+                <div className="flex icons ">
+                  <div style={{ gap: "18px" }} className="flex ">
+                    <a href="">
+                      <div className="icon-link" />
+                    </a>
+                    <a href="">
+                      <div className="icon-github" />
+                    </a>
+                  </div>
+
+                  <a className="link flex" href="">
+                    more
+                    <span className="icon-arrow-right"></span>
+                  </a>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
